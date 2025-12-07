b364b5f6171e1c19a59f864bb5bb7b5f98f8f7bf
alexanderxhaufe-source <alexanderxhaufe@arizona.edu>
2025-12-06 17:21:06 -0700

    Added query3 to temp code file

---
+++ QueryHold.java
@@ -0,0 +1,106 @@
+import java.io.*;
+import java.sql.*;
+import java.util.Scanner;
+import java.lang.Math;
+import java.time.LocalDate;
+public class QueryHold {
+        public static void main (String[] args) {
+                final String oracleURL = "jdbc:oracle:thin:@aloe.cs.arizona.edu:1521:oracle";
+
+                String query = "";
+
+                String username = "alexanderxhaufe";
+                String password = "a7394";
+
+                if (args.length == 2) {
+                        username = args[0];
+                        password = args[1];
+                }
+
+                try {
+                        Class.forName("oracle.jdbc.OracleDriver");
+
+                } catch (ClassNotFoundException e) {
+                        System.err.println("*** ClassNotFoundException, error loading JDBC driver\n");
+                        System.exit(-1);
+                }
+
+                Connection dbconn = null;
+
+                try {
+                        dbconn = DriverManager.getConnection(oracleURL,username,password);
+                } catch (SQLException e) {
+                        System.err.println("**** SQLException: could not open connection.");
+                        System.err.println("\tMessage:  "+e.getMessage());
+                        System.err.println("\tSQLState: "+e.getSQLState());
+                        System.err.println("\tErrorCode:        "+e.getErrorCode());
+                        System.exit(-1);
+                }
+                //BELOW HERE the db is connected
+                Statement stmt = null;
+                ResultSet answer = null;
+
+                Scanner stream = new Scanner(System.in);
+                try {
+                        while (true) {
+                                System.out.println("\n This is a placeholder file to test queries\n"+
+                                                "Type 3 for query 3 (upcoming events with openings)\n"+
+                                                "Type 5 to exit");
+                                int selected = stream.nextInt();
+                                stream.nextLine();
+                                switch(selected) {
+                                        case 1:
+                                                break;
+                                        case 2:
+                                                break;
+                                        case 3:
+                                                // CASE 3 is to list all upcoming events that have available capacity
+                                                query = """
+                                                        select Event.*,Room.location,(select count(*)
+                                                                        from EventBooking
+                                                                        where EventBooking.eventID = Event.eventID)
+                                                                        as registered
+                                                        from Event,Room
+                                                        where Event.roomID = Room.roomID
+
+                                                        """;
+                                                stmt = dbconn.createStatement();
+                                                answer = stmt.executeQuery(query);
+
+                                                System.out.println("The following events have available capacity\n");
+                                                //String toPrint = "";
+                                                while(answer.next()){
+                                                        System.out.println("Event Name: "+answer.getString("eventName")
+                                                                        +" | Date: "+answer.getDate("eventDate")
+                                                                        +" | Time: "+answer.getString("time")
+                                                                        +" | Room Location: "+answer.getString("location")
+                                                                        +" | Registered Attendees: "+answer.getInt("registered")
+                                                                        +" | Maximum Capacity: "+answer.getInt("maxCapacity")
+                                                                        +" | Coordinator: "+answer.getString("description")
+                                                                        );
+                                                }
+                                                System.out.println();
+                                                break;
+                                        case 4:
+                                                break;
+                                        case 5:
+                                                stream.close();
+                                                stmt.close();
+                                                dbconn.close();
+                                                return;
+
+                                        default:
+                                                System.out.println("only enter 1-5");
+                                                continue;
+                                }
+                                }
+                        } catch (SQLException e) {
+                                System.err.println("*** SQLException:");
+                                System.err.println("\tMessage:  "+e.getMessage());
+                                System.err.println("\tSQLState: "+e.getSQLState());
+                                System.err.println("\tErrorCode:        "+e.getErrorCode());
+                                System.exit(-1);
+                        }
+                }
+        }
+
