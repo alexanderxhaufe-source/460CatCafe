2e7eeab392db1e85bac136778268a8a62146a46a
Aleksei <clooune@proton.me>
2025-12-07 22:50:21 -0700

    modify table 4/7 completed

--- ModifyTable.java
+++ ModifyTable.java
@@ -156,7 +156,97 @@ public class ModifyTable {
     }
 
     private static void modHR(Scanner scanner, Connection conn) {
-        
+        try {
+            String updateSQL = "update HealthRecord set description = ? where appID = ?";
+            // get recordID to modify from the user and convert it to int
+            PreparedStatement pstmt = conn.prepareStatement(updateSQL);
+            String recID = Common.inputNumber(scanner, "Enter the recordID of the healthrecord to modify: ").trim();
+            int recIDInt = Integer.parseInt(recID);
+            String newDescription = Common.inputString(scanner, "Enter new health record information: ");
+            pstmt.setString(1, newDescription);
+            pstmt.setInt(2, recIDInt);
+            int rowsChanged = pstmt.executeUpdate();
+            System.out.println(rowsChanged + " row(s) updated.");
+            pstmt.close();
+        } catch (SQLException e) {
+            System.out.println("SQL Error: " + e.getMessage());
+        }
+    }
+
+    private static void modMember(Scanner scanner, Connection conn) {
+        try {
+            String memID = Common.inputNumber(scanner, "Enter the memberID of the customer to modify: ").trim();
+            int memIDInt = Integer.parseInt(memID);
+            String query = "select firstName || \' \' || lastName \"custName\" from Member where memberID = " + memIDInt;
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            if (!rs.next()) {
+                System.out.println("No record found with that event booking ID.");
+                rs.close();
+                stmt.close();
+                return;
+            }
+            String custName = rs.getString("custName"); // customer full name
+            rs.close();
+            stmt.close();
+            int userChoice;
+            System.out.println("Customer Name: " + custName);
+            System.out.println("1. Update Contact info");
+            System.out.println("2. Modify membership tier");
+            System.out.print("Enter a choice (1-2): ");
+            try {
+                userChoice = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid: Must be a number\n");
+                scanner.next(); // move scanner past invalid input
+                return;
+            }
+            if (userChoice < 1 || userChoice > 2) { // invalid input
+                System.out.println("Invalid: Must be between 1 and 2\n");
+                return;
+            }
+            if (userChoice == 1) { // checkin
+                String updateSQL = "update Member set phoneNoAreaCode = ? phoneNo = ? where memberID = ?";
+                PreparedStatement pstmt = conn.prepareStatement(updateSQL);
+                String fullPhoneNo = Common.inputNumber(scanner, "Enter new phone number (only digits): ");
+                if (fullPhoneNo.length() != 10) {
+                    System.out.println("Phone numbers must be 10 digits.");
+                    pstmt.close();
+                }
+                int areaCode = Integer.parseInt(fullPhoneNo.substring(0, 3)); // first 3 digits 
+                int phoneNo = Integer.parseInt(fullPhoneNo.substring(3)); // last 7 digits
+                pstmt.setInt(1, areaCode);
+                pstmt.setInt(2, phoneNo);
+                pstmt.setInt(3, memIDInt);
+                int rowsChanged = pstmt.executeUpdate(); // execute update and get no rows changed
+                System.out.println(rowsChanged + " row(s) updated.");
+                pstmt.close();
+                return;
+            }
+            String updateMembership = "update Member set membershipTier = ? where memberID = ?";
+            System.out.println("Update Membership tier to:\n1. Day pass\n2. Monthly\n3. Annual");
+            int memChoice;
+            try {
+                memChoice = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid: Must be a number\n");
+                scanner.next(); // move scanner past invalid input
+                return;
+            }
+            if (memChoice < 1 || memChoice > 3) { // invalid input
+                System.out.println("Invalid: Must be between 1 and 3\n");
+                return;
+            }
+            PreparedStatement pstmt = conn.prepareStatement(updateMembership);
+            pstmt.setInt(1, memChoice);
+            pstmt.setInt(2, memIDInt);
+            int rowsChanged = pstmt.executeUpdate(); // execute update and get no rows changed
+            System.out.println(rowsChanged + " row(s) updated.");
+            pstmt.close();
+            return;
+        } catch (SQLException e) {
+            System.out.println("SQL Error: " + e.getMessage());
+        }
     }
 	
 	public static void modTable(int tableValue, Scanner scanner, Connection conn) {
@@ -168,11 +258,12 @@ public class ModifyTable {
                 modEB(scanner, conn);
 				break;
 			case 3: // modify health record
-
+                modHR(scanner, conn);
 				break;
-			case 4:
+			case 4: // modify member
+                modMember(scanner, conn);
 				break;
-			case 5:
+			case 5: // modify pet
 				break;
 			case 6:
 				break;
