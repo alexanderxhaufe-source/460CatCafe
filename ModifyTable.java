b77a107712aab4b9c818158f6955461d355bee01
Aleksei <clooune@proton.me>
2025-12-08 01:03:57 -0700

    finished modifyTable

--- ModifyTable.java
+++ ModifyTable.java
@@ -78,6 +78,16 @@ public class ModifyTable {
         }
     }
 
+    /*
+    * Method: modEB
+    * Purpose: Modify a record in the event booking table
+    * Parameters: Scanner scanner - the scanner to read user input
+    * Connection conn - the connection to the database
+    * Preconditions: connection and scanner are valid
+    * Postconditions: a record in the event booking table is modified or
+    * an error message is displayed
+    * Returns: void
+    */
     private static void modEB(Scanner scanner, Connection conn) {
         try {
             String EBID = Common.inputNumber(scanner, "Enter the event booking ID of the record to modify: ").trim();
@@ -154,10 +164,19 @@ public class ModifyTable {
             System.out.println("SQL Error: " + e.getMessage());
         }
     }
-
+    /*
+    * Method: modHR
+    * Purpose: Modify a record in the HealthRecord
+    * Parameters: Scanner scanner - the scanner to read user input
+    * Connection conn - the connection to the database
+    * Preconditions: connection and scanner are valid
+    * Postconditions: a record in the HealthRecord table is modified or
+    * an error message is displayed
+    * Returns: void
+    */
     private static void modHR(Scanner scanner, Connection conn) {
         try {
-            String updateSQL = "update HealthRecord set description = ? where appID = ?";
+            String updateSQL = "update HealthRecord set description = ? where recordID = ?";
             // get recordID to modify from the user and convert it to int
             PreparedStatement pstmt = conn.prepareStatement(updateSQL);
             String recID = Common.inputNumber(scanner, "Enter the recordID of the healthrecord to modify: ").trim();
@@ -173,6 +192,16 @@ public class ModifyTable {
         }
     }
 
+    /*
+    * Method: modMember
+    * Purpose: Modify a record in the member table
+    * Parameters: Scanner scanner - the scanner to read user input
+    * Connection conn - the connection to the database
+    * Preconditions: connection and scanner are valid
+    * Postconditions: a record in the member table is modified or
+    * an error message is displayed
+    * Returns: void
+    */
     private static void modMember(Scanner scanner, Connection conn) {
         try {
             String memID = Common.inputNumber(scanner, "Enter the memberID of the customer to modify: ").trim();
@@ -206,7 +235,7 @@ public class ModifyTable {
                 return;
             }
             if (userChoice == 1) { // checkin
-                String updateSQL = "update Member set phoneNoAreaCode = ? phoneNo = ? where memberID = ?";
+                String updateSQL = "update Member set phoneNoAreaCode = ?, phoneNo = ? where memberID = ?";
                 PreparedStatement pstmt = conn.prepareStatement(updateSQL);
                 String fullPhoneNo = Common.inputNumber(scanner, "Enter new phone number (only digits): ");
                 if (fullPhoneNo.length() != 10) {
@@ -248,7 +277,246 @@ public class ModifyTable {
             System.out.println("SQL Error: " + e.getMessage());
         }
     }
+
+    /*
+    * Method: modPet
+    * Purpose: Modify a record in the Pet table
+    * Parameters: Scanner scanner - the scanner to read user input
+    * Connection conn - the connection to the database
+    * Preconditions: connection and scanner are valid
+    * Postconditions: a record in the Pet table is modified or
+    * an error message is displayed
+    * Returns: void
+    */
+    private static void modPet(Scanner scanner, Connection conn) {
+        try {
+            String petID = Common.inputNumber(scanner, "Enter the petID of the pet to modify: ").trim();
+            int petIDInt = Integer.parseInt(petID);
+            String query = "select name from alexanderxhaufe.Pet where petID = " + petIDInt;
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            if (!rs.next()) {
+                System.out.println("No record found with that pet ID.");
+                rs.close();
+                stmt.close();
+                return;
+            }
+            String petName = rs.getString("name");
+            rs.close();
+            stmt.close();
+            int userChoice;
+            System.out.println("Pet Name: " + petName);
+            System.out.println("1. Update age");
+            System.out.println("2. Update temperament");
+            System.out.println("3. Update current status");
+            System.out.print("Enter a choice (1-3): ");
+            try {
+                userChoice = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid: Must be a number\n");
+                scanner.next(); // move scanner past invalid input
+                return;
+            }
+            if (userChoice < 1 || userChoice > 3) { // invalid input
+                System.out.println("Invalid: Must be between 1 and 3\n");
+                return;
+            }
+            scanner.nextLine();
+            switch (userChoice) {
+                case 1: // update age
+                    String newAge = Common.inputNumber(scanner, "Enter the age of the pet: ");
+                    int newAgeInt = Integer.parseInt(newAge);
+                    String update = "update Pet set age = ? where petID = ?";
+                    PreparedStatement pstmt = conn.prepareStatement(update);
+                    pstmt.setInt(1, newAgeInt);
+                    pstmt.setInt(2, petIDInt);
+                    int rowsChanged = pstmt.executeUpdate(); // execute update and get no rows changed
+                    System.out.println(rowsChanged + " row(s) updated.");
+                    pstmt.close();
+                    break;
+                case 2: // update temperament
+                    String newTemp = Common.inputString(scanner, "Enter the temperament of the pet: ");
+                    String update2 = "update Pet set temperament = ? where petID = ?";
+                    PreparedStatement pstmt2 = conn.prepareStatement(update2);
+                    pstmt2.setString(1, newTemp);
+                    pstmt2.setInt(2, petIDInt);
+                    int rowsChanged2 = pstmt2.executeUpdate(); // execute update and get no rows changed
+                    System.out.println(rowsChanged2 + " row(s) updated.");
+                    pstmt2.close();
+                    break;
+                case 3: // update current status
+                    String newStatus = Common.inputString(scanner, "Enter the status of the pet: ");
+                    String update3 = "update Pet set currStat = ? where petID = ?";
+                    PreparedStatement pstmt3 = conn.prepareStatement(update3);
+                    pstmt3.setString(1, newStatus);
+                    pstmt3.setInt(2, petIDInt);
+                    int rowsChanged3 = pstmt3.executeUpdate(); // execute update and get no rows changed
+                    System.out.println(rowsChanged3 + " row(s) updated.");
+                    pstmt3.close();
+                    break;
+            }
+        } catch (SQLException e) {
+            System.out.println("SQL Error: " + e.getMessage());
+        }
+    }
+
+    /*
+    * Method: modRes
+    * Purpose: Modify a record in the Reservation table
+    * Parameters: Scanner scanner - the scanner to read user input
+    * Connection conn - the connection to the database
+    * Preconditions: connection and scanner are valid
+    * Postconditions: a record in the Reservation table is modified or
+    * an error message is displayed
+    * Returns: void
+    */
+    private static void modRes(Scanner scanner, Connection conn) {
+        try {
+            String resID = Common.inputNumber(scanner, "Enter the reservationID to modify: ").trim();
+            int resIDInt = Integer.parseInt(resID);
+            String query = "select customerID from alexanderxhaufe.Reservation where reservationID = " + resIDInt;
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            if (!rs.next()) {
+                System.out.println("No record found with that reservation ID.");
+                rs.close();
+                stmt.close();
+                return;
+            }
+            rs.close();
+            stmt.close();
+            
+            int userChoice;
+            System.out.println("1. Update reservation date");
+            System.out.println("2. Extend duration");
+            System.out.println("3. Update customer status (in or out)");
+            System.out.print("Enter a choice (1-3): ");
+            try {
+                userChoice = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid: Must be a number\n");
+                scanner.next();
+                return;
+            }
+            if (userChoice < 1 || userChoice > 3) {
+                System.out.println("Invalid: Must be between 1 and 3\n");
+                return;
+            }
+            scanner.nextLine();
+            switch (userChoice) {
+                case 1:
+                    String newDate = Common.inputString(scanner, "Enter new reservation date (YYYY-MM-DD): ");
+                    String newTime = Common.inputString(scanner, "Enter new reservation time (HH:MM AM/PM): ");
+                    String updateDate = "update Reservation set resDate = TO_DATE(?, 'YYYY-MM-DD'), startTime = ? where reservationID = ?";
+                    PreparedStatement pstmt = conn.prepareStatement(updateDate);
+                    pstmt.setString(1, newDate);
+                    pstmt.setString(1, newTime);
+                    pstmt.setInt(2, resIDInt);
+                    int rowsChanged = pstmt.executeUpdate();
+                    System.out.println(rowsChanged + " row(s) updated.");
+                    pstmt.close();
+                    break;
+                case 2:
+                    String newDuration = Common.inputString(scanner, "Enter new duration (HH:MM): ");
+                    String updateDuration = "update Reservation set duration = ? where reservationID = ?";
+                    PreparedStatement pstmt2 = conn.prepareStatement(updateDuration);
+                    pstmt2.setString(1, newDuration);
+                    pstmt2.setInt(2, resIDInt);
+                    int rowsChanged2 = pstmt2.executeUpdate();
+                    System.out.println(rowsChanged2 + " row(s) updated.");
+                    pstmt2.close();
+                    break;
+                case 3:
+                    String newStatus = Common.inputNumber(scanner, "Enter status (1=in, 0=out): ").trim();
+                    int newStatusInt = Integer.parseInt(newStatus);
+                    if (newStatusInt < 0 || newStatusInt > 1) {
+                        System.out.println("Invalid: Must be 1 or 0\n");
+                        return;
+                    }
+                    String updateStatus = "update Reservation set inStatus = ? where reservationID = ?";
+                    PreparedStatement pstmt3 = conn.prepareStatement(updateStatus);
+                    pstmt3.setInt(1, newStatusInt);
+                    pstmt3.setInt(2, resIDInt);
+                    int rowsChanged3 = pstmt3.executeUpdate();
+                    System.out.println(rowsChanged3 + " row(s) updated.");
+                    pstmt3.close();
+                    break;
+            }
+        } catch (SQLException e) {
+            System.out.println("SQL Error: " + e.getMessage());
+        }
+    }
+    /*
+    * Method: modOrder
+    * Purpose: Modify a record in the TotalOrder table
+    * Parameters: Scanner scanner - the scanner to read user input
+    * Connection conn - the connection to the database
+    * Preconditions: connection and scanner are valid
+    * Postconditions: a record in the TotalOrder table is modified or
+    * an error message is displayed
+    * Returns: void
+    */
+    private static void modOrder(Scanner scanner, Connection conn) {
+        try {
+            String orderID = Common.inputNumber(scanner, "Enter the orderID to modify: ").trim();
+            int orderIDInt = Integer.parseInt(orderID);
+            int userChoice;
+            System.out.println("1. Update order status");
+            System.out.println("2. Update payment status");
+            System.out.print("Enter a choice (1-2): ");
+            try {
+                userChoice = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid: Must be a number\n");
+                scanner.next();
+                return;
+            }
+            if (userChoice < 1 || userChoice > 2) {
+                System.out.println("Invalid: Must be between 1 and 2\n");
+                return;
+            }
+            scanner.nextLine();
+            if (userChoice == 1) {
+                String newStatus = Common.inputString(scanner, "Enter new order status (placed/prepared/delivered): ");
+                String updateStatus = "update TotalOrder set orderStatus = ? where orderID = ?";
+                PreparedStatement pstmt = conn.prepareStatement(updateStatus);
+                pstmt.setString(1, newStatus);
+                pstmt.setInt(2, orderIDInt);
+                int rowsChanged = pstmt.executeUpdate();
+                System.out.println(rowsChanged + " row(s) updated.");
+                pstmt.close();
+            } else {
+                String newPstatus = Common.inputNumber(scanner, "Enter new payment status (0: unpaid, 1: paid): ");
+                int newPstatusint = Integer.parseInt(newPstatus);
+                if (newPstatusint < 0 || newPstatusint > 1) {
+                    System.out.println("Invalid: Must be 1 or 0\n");
+                    return;
+                }
+                String updatePstatus = "update TotalOrder set paymentStatus = ? where orderID = ?";
+                PreparedStatement pstmt = conn.prepareStatement(updatePstatus);
+                pstmt.setInt(1, newPstatusint);
+                pstmt.setInt(2, orderIDInt);
+                int rowsChanged = pstmt.executeUpdate();
+                System.out.println(rowsChanged + " row(s) updated.");
+                pstmt.close();
+            }
+        } catch (SQLException e) {
+            System.out.println("SQL Error: " + e.getMessage());
+        }
+    }
 	
+    /*
+    * Method: modTable
+    * Purpose: Modify records in the database
+    * Parameters: Scanner scanner - the scanner to read user input
+    * Connection conn - the connection to the database
+    * int TableValue - the selected table to modify
+    * Preconditions: connection and scanner are valid and tableValue is 
+    * in the range of possible choices.
+    * Postconditions: a table is either modified or not depending on whether
+    * or not the user makes an incorrect input or an SQL error is returned
+    * Returns: void
+    */
 	public static void modTable(int tableValue, Scanner scanner, Connection conn) {
 		switch (tableValue) {
 			case 1: // modify adoption table
@@ -264,10 +532,13 @@ public class ModifyTable {
                 modMember(scanner, conn);
 				break;
 			case 5: // modify pet
+                modPet(scanner, conn);
 				break;
-			case 6:
+			case 6: // modify reservation
+                modRes(scanner, conn);
 				break;
-			case 7:
+			case 7: // modify totalorder
+                modOrder(scanner, conn);
 				break;
 			default:
 				System.out.println("Invalid Value in addToTable()");
